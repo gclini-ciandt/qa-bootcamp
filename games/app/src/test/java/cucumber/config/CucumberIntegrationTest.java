c81607bb29b82853eeea8d04a27f0cb3cda2f0be
Gilcimara Pinho Clini <CIANDT\gclini@lnb028608cps.ciandt.global>
2021-05-25 15:32:49 -0300

    BTCT-3 Implementing get list of all games

--- games/app/src/test/java/cucumber/config/CucumberIntegrationTest.java
+++ games/app/src/test/java/cucumber/config/CucumberIntegrationTest.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions (
         features = { "src/test/resources/features"}, //Identifies either a directory containing feature files
-        glue = { "classpath:cucumber.step.definitions", "classpath:cucumber.step.hooks"}, //glue code (step definitions, hooks and plugins)
+        glue = { "classpath:cucumber.step.definitions", "classpath:cucumber.step.hooks"} ,//glue code (step definitions, hooks and plugins)
         strict=true
 )
 public class CucumberIntegrationTest {
--- games/app/src/test/resources/features/Games.feature
+++ games/app/src/test/resources/features/Games.feature
@@ -4,3 +4,13 @@ Feature: Games
     When I play from the top
     Then I get two cards from the top from a shuffled deck
     And game cards are saved
+
+  Scenario: Should get two cards from the bottom of a shuffled deck
+    When I play from the bottom
+    Then I get two cards from the bottom from a shuffled deck
+    And game cards are saved
+
+  Scenario: Should get a list of all games
+    Given I have played games
+    When I ask for all the games
+    Then I get a list of all played games
